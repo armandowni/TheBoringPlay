c0e32ffc8886dc965653760045d3cef3c17eb30c
armandowni <56640858+armandowni@users.noreply.github.com>
2020-11-21 20:08:50 +0700

    version baru coy

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Home msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
+
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './component/HelloWorld.vue'
+import Home from "./components/home/Home.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Home
+  },
+};
 </script>
 
 <style>
---
+++ src/components/canon-game/CanonGame.vue
@@ -0,0 +1,3 @@
+<template src="./CanonGame.html"></template>
+<style src="./CanonGame.css"></style>
+<script src="./CanonGame.js"></script>
