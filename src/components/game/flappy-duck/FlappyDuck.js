ebba8cf85324fa954ce6c0e0fd982a18eef6cc06
armandowni <armando.nurimansyah@gmail.com>
2021-01-09 07:12:50 +0700

    newGame

--- src/components/game/flappy-duck/FlappyDuck.js
+++ src/components/game/flappy-duck/FlappyDuck.js
@@ -1,11 +1,10 @@
 let myGameArea = {
     start: function(gameCanvas) {
-        gameCanvas.width = 640;
-        gameCanvas.height = 400;
+        this.width = gameCanvas.width = 640;
+        this.height = gameCanvas.height = 400;
         this.context = gameCanvas.getContext("2d");
-        // document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;
-        // this.interval = setInterval(updateGameArea, 20);
+        this.interval = 0;
     },
     view: function(gameCanvas) {
         gameCanvas.width = 640;
@@ -15,8 +14,16 @@ let myGameArea = {
     },
     clear: function(gameCanvas) {
         this.context.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+    },
+    stop: function() {
+        clearInterval(this.interval);
+        this.context.filter = 'contrast(2)'
+        this.context.font = "50px Georgia";
+        this.context.fillText("Crash!", 250, 200);
     }
 }
+
+import image from '@/assets/image/flappy_duck-removebg.png'
 
 export default {
     name: 'FlappyDuck',
@@ -25,7 +32,10 @@ export default {
             myGamePiece: null,
             myObstacles: [],
             myScore: null,
+            duck: image,
             gameCanvas: null,
+            interval: null,
+            disableBtnJump: true,
         }
     },
     mounted() {
@@ -34,19 +44,34 @@ export default {
     },
     methods: {
         viewGame() {
-            this.myGamePiece = new this.component(30, 30, "red", 10, 120);
-            this.myGamePiece.gravity = 0.05;
+            this.myGamePiece = new this.component(50, 50, this.duck, 10, 150, 'image');
             this.myScore = new this.component("30px", "Consolas", "black", 280, 40, "text");
+            this.myObstacles.push(new this.component(50, 160, "green", 500, 0));
+            this.myObstacles.push(new this.component(50, 160, "green", 500, 250));
+            this.disableBtnJump = true
+
             myGameArea.view(this.gameCanvas)
+            this.myScore.text = "SCORE: " + myGameArea.frameNo;
+            this.myScore.update();
+            this.myGamePiece.update();
+            this.myObstacles[0].update();
+            this.myObstacles[1].update();
         },
         startGame() {
-            this.myGamePiece = new this.component(30, 30, "red", 10, 120);
+            this.myGamePiece = new this.component(50, 50, this.duck, 10, 150, 'image');
             this.myGamePiece.gravity = 0.05;
             this.myScore = new this.component("30px", "Consolas", "black", 280, 40, "text");
+            this.disableBtnJump = false
             myGameArea.start(this.gameCanvas);
+            myGameArea.interval = setInterval(this.updateGameArea, 20);
+            this.updateGameArea();
         },
         component(width, height, color, x, y, type) {
             this.type = type;
+            if (type == "image") {
+                this.image = new Image();
+                this.image.src = color;
+            }
             this.score = 0;
             this.width = width;
             this.height = height;
@@ -62,6 +87,11 @@ export default {
                     ctx.font = this.width + " " + this.height;
                     ctx.fillStyle = color;
                     ctx.fillText(this.text, this.x, this.y);
+                } else if (type == "image") {
+                    ctx.drawImage(this.image,
+                        this.x,
+                        this.y,
+                        this.width, this.height);
                 } else {
                     ctx.fillStyle = color;
                     ctx.fillRect(this.x, this.y, this.width, this.height);
@@ -74,7 +104,7 @@ export default {
                 this.hitBottom();
             }
             this.hitBottom = function() {
-                let rockbottom = myGameArea.canvas.height - this.height;
+                let rockbottom = myGameArea.height - this.height;
                 if (this.y > rockbottom) {
                     this.y = rockbottom;
                     this.gravitySpeed = 0;
@@ -101,21 +131,24 @@ export default {
             let x, height, gap, minHeight, maxHeight, minGap, maxGap;
             for (let i = 0; i < this.myObstacles.length; i += 1) {
                 if (this.myGamePiece.crashWith(this.myObstacles[i])) {
+                    myGameArea.stop();
+                    this.disableBtnJump = true
                     return;
                 }
             }
-            myGameArea.clear();
+            myGameArea.clear(this.gameCanvas);
             myGameArea.frameNo += 1;
             if (myGameArea.frameNo == 1 || this.everyinterval(150)) {
-                x = myGameArea.canvas.width;
+                // console.log("width:" + myGameArea.width);
+                x = myGameArea.width;
                 minHeight = 20;
                 maxHeight = 200;
                 height = Math.floor(Math.random() * (maxHeight - minHeight + 1) + minHeight);
                 minGap = 50;
                 maxGap = 200;
                 gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
-                this.myObstacles.push(new this.component(10, height, "green", x, 0));
-                this.myObstacles.push(new this.component(10, x - height - gap, "green", x, height + gap));
+                this.myObstacles.push(new this.component(50, height, "green", x, 0));
+                this.myObstacles.push(new this.component(50, x - height - gap, "green", x, height + gap));
             }
             for (let i = 0; i < this.myObstacles.length; i += 1) {
                 this.myObstacles[i].x += -1;
@@ -132,6 +165,7 @@ export default {
         },
         jump(n) {
             this.myGamePiece.gravity = n;
+            // console.log(this.myGamePiece.gravity);
         }
     },
 };
