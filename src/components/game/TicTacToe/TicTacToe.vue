43c67f50fae4d385e2f925558389ff76de14b779
armandowni <armando.nurimansyah@gmail.com>
2021-03-09 20:01:35 +0700

    new game

---
+++ src/components/game/TicTacToe/TicTacToe.vue
@@ -0,0 +1,7 @@
+
+<template>
+    <div>Testing</div>
+</template>
+
+<script src="./TicTacToe.js"></script>
+<style scoped src="./TicTacToe.css"></style>
--- src/components/game/routeGame.js
+++ src/components/game/routeGame.js
@@ -1,5 +1,6 @@
 import CanonGame from './canon-game/CanonGame.vue'
 import FlappyDuck from './flappy-duck/FlappyDuck.vue'
+import TicTacToe from './TicTacToe/TicTacToe.vue'
 
 const routeGame = [{
         path: '/gameList/canonGame',
@@ -10,6 +11,11 @@ const routeGame = [{
         path: '/gameList/flappyduck',
         name: 'Flappy Duck',
         component: FlappyDuck
+    },
+    {
+        path: '/gameList/tictactoe',
+        name: 'Tic Tac Toe',
+        component: TicTacToe
     },
 ]
 export default routeGame;
