af058b8fd8a349fd07a178b30b09dac3619f69ae
Duckcanfly <armando.nurimansyah@gmail.com>
2024-01-25 19:07:42 +0700

    fixing

--- src/router/restAPI.js
+++ src/router/restAPI.js
@@ -1,7 +1,7 @@
-let localStorage = "http://localhost:3000"
-let globalStorage = "https://theboringplayapi.herokuapp.com"
+const localStorage = "http://localhost:3000";
+const globalStorage = process.env.VUE_APP_URL_API || localStorage;
 
 export default {
-    localStorage,
-    globalStorage
-}
+  localStorage,
+  globalStorage,
+};
